d6af26d3d9c7ef51f55e0587f7544f27fae5514c
keven.barauna <keven.barauna@SLT-002251>
2019-11-07 11:08:53 -0300

    Enviar frase pelo E-mail

---
+++ Email/Email/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFrase = new System.Windows.Forms.Button();
+            this.BtnHtml = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFrase
+            // 
+            this.BtnFrase.Location = new System.Drawing.Point(39, 53);
+            this.BtnFrase.Name = "BtnFrase";
+            this.BtnFrase.Size = new System.Drawing.Size(109, 49);
+            this.BtnFrase.TabIndex = 0;
+            this.BtnFrase.Text = "Enviar Frase";
+            this.BtnFrase.UseVisualStyleBackColor = true;
+            this.BtnFrase.Click += new System.EventHandler(this.BtnFrase_Click);
+            // 
+            // BtnHtml
+            // 
+            this.BtnHtml.Location = new System.Drawing.Point(39, 121);
+            this.BtnHtml.Name = "BtnHtml";
+            this.BtnHtml.Size = new System.Drawing.Size(109, 49);
+            this.BtnHtml.TabIndex = 1;
+            this.BtnHtml.Text = "Enviar HTML";
+            this.BtnHtml.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 237);
+            this.Controls.Add(this.BtnHtml);
+            this.Controls.Add(this.BtnFrase);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "E-mail";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFrase;
+        private System.Windows.Forms.Button BtnHtml;
+    }
+}
+
---
+++ Email/Email/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;//
+using System.Net.Mail;//
+
+namespace Email
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFrase_Click(object sender, EventArgs e)
+        {
+            //Enviar E-mail pelo C#
+            SmtpClient cliente = new SmtpClient();
+            NetworkCredential credenciais = new NetworkCredential();
+
+            //CONFIGURAÇÕES DO CLIENTE
+            cliente.Host = "smtp.gmail.com";
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+            cliente.DeliveryMethod = SmtpDeliveryMethod.Network;
+            cliente.UseDefaultCredentials = false;
+
+            //DEFINIT ACESSO E-MAIL
+            credenciais.UserName = "baianofotografia.keven";
+            credenciais.Password = "12345678kel";
+
+            //CREDENCIAIS NO CLIENTE
+            cliente.Credentials = credenciais;
+
+            //MENSAGEM
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("baianofotografia.keven@gmail.com");
+            mensagem.Subject = "Títullo da mensagem";
+            mensagem.Body = "Essa é a mensagem que estou enviando";
+            mensagem.To.Add("keven.barauna@solutis.com.br");
+
+            //ENVIAR
+            cliente.Send(mensagem);
+            MessageBox.Show("OK");
+
+        }
+    }
+}
